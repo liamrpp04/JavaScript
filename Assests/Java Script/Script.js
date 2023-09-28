0a8e58aabf8748e728b3b1cc7de0f08c7d23f2fa
liamrpp04 <liamrpp04@gmail.com>
2023-09-28 13:11:33 -0500

    primeros pasos

---
+++ Assests/Java Script/Script.js
@@ -0,0 +1,30 @@
+var canvas = document.getElementById("juego");
+var contexto = canvas.getContext("2d");
+/*contexto.fillStyle = "rgb(200,0,0)";
+contexto.fillRect(100,100,200,200);
+contexto.strokeStyle = "purple";
+contexto.strokeRect(100, 100, 200, 200);
+contexto.strokeStyle = "orange";
+contexto.lineWidth = "50";
+contexto.fillStyle = "red";
+
+contexto.rect(110, 110, 40, 80);
+contexto.stroke();
+contexto.fill();
+contexto.strokeStyle = "purple";
+contexto.beginPath();
+contexto.moveTo(20, 20);
+contexto.lineTo(100, 20);
+contexto.lineTo(50, 60);
+contexto.lineTo(50, 100);
+contexto.closePath();
+
+contexto.stroke();
+contexto.strokeStyle = "green";
+contexto.fillStyle = "yellow";
+
+contexto.beginPath();
+contexto.arc(120, 110, 40, 0, 2 * Math.PI);
+
+contexto.stroke();
+contexto.fill();*/
